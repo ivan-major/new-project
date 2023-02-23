fcfa7c6cb329b4c04b2c710eb8a1c6f0f5a192ab
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2023-02-23 10:54:25 +0200

    123

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/new-project/'
+// })
+module.exports = {
+  // transpileDependencies: true,
   publicPath: '/new-project/'
-})
+}
